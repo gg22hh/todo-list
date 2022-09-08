c1598843ff3f5302e7fcf4423b08850546c9262f
g322h <g2203h@gmail.com>
2022-09-08 18:18:51 +0300

    test

--- src/components/NewTodoForm.tsx
+++ src/components/NewTodoForm.tsx
@@ -3,25 +3,37 @@ import { useAppDispatch } from "../hooks";
 import { addNewTodo } from "../store/TodoSlice";
 
 export const NewTodoForm: FC = () => {
-    const [newTodoValue, setNewTodoValue] = useState<string>("");
-    const dispatch = useAppDispatch();
+   const [newTodoValue, setNewTodoValue] = useState<string>("");
+   const dispatch = useAppDispatch();
 
-    const addTodo: React.FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
-        if (newTodoValue) {
-            dispatch(addNewTodo(newTodoValue));
-        }
-        setNewTodoValue("");
-    };
-    return (
-        <form className="todo__form" onSubmit={addTodo}>
-            <input
-                type="text"
-                placeholder="New task"
-                value={newTodoValue}
-                onChange={(e) => setNewTodoValue(e.target.value)}
-            />
-            <button type="submit">Add</button>
-        </form>
-    );
+   const addTodo: React.FormEventHandler<HTMLFormElement> = async (e) => {
+      e.preventDefault();
+      if (newTodoValue) {
+         // dispatch(addNewTodo(newTodoValue));
+         const newTodo = {
+            title: newTodoValue,
+            completed: false,
+         };
+
+         const response = await fetch("http://localhost:3001/todos", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTodo),
+         });
+      }
+      setNewTodoValue("");
+   };
+   return (
+      <form className="todo__form" onSubmit={addTodo}>
+         <input
+            type="text"
+            placeholder="New task"
+            value={newTodoValue}
+            onChange={(e) => setNewTodoValue(e.target.value)}
+         />
+         <button type="submit">Add</button>
+      </form>
+   );
 };
--- src/store/TodoSlice.ts
+++ src/store/TodoSlice.ts
@@ -2,99 +2,99 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { ITodos } from "../types/types";
 
 type Todo = {
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
+   userId: number;
+   id: number;
+   title: string;
+   completed: boolean;
 };
 
 type TodosState = {
-    todos: Todo[];
-    loading: boolean;
+   todos: Todo[];
+   loading: boolean;
 };
 
 const initialState: TodosState = {
-    todos: [],
-    loading: false,
+   todos: [],
+   loading: false,
 };
 
 export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    if (response.ok) {
-        const json = await response.json();
-        return json.slice(0, 3);
-    } else {
-        return new Error("Ошибка при получении todos");
-    }
+   const response = await fetch("http://localhost:3001/todos/");
+   if (response.ok) {
+      const json = await response.json();
+      return json;
+   } else {
+      return new Error("Ошибка при получении todos");
+   }
 });
 
 const todoSlice = createSlice({
-    name: "todos",
-    initialState,
-    reducers: {
-        toggleItem(state, action: PayloadAction<ITodos>) {
-            const updatedTodo = {
-                ...action.payload,
-                completed: !action.payload.completed,
-            };
-            state.todos = state.todos.map((todo) => {
-                if (todo.id === updatedTodo.id) {
-                    return updatedTodo;
-                }
-                return todo;
-            });
-        },
-        deleteTodos(state) {
-            const sure = window.confirm("Delete tasks ?");
-            if (sure) {
-                const updatedTodos = [...state.todos];
-                state.todos = updatedTodos.filter(
-                    (todo) => todo.completed === false
-                );
+   name: "todos",
+   initialState,
+   reducers: {
+      toggleItem(state, action: PayloadAction<ITodos>) {
+         const updatedTodo = {
+            ...action.payload,
+            completed: !action.payload.completed,
+         };
+         state.todos = state.todos.map((todo) => {
+            if (todo.id === updatedTodo.id) {
+               return updatedTodo;
             }
-        },
-        deleteTodo(state, action: PayloadAction<number>) {
-            const sure = window.confirm("Delete task ?");
-            if (sure) {
-                const updatedTodo = { ...state.todos[action.payload] };
-                state.todos = state.todos.filter(
-                    (todo) => todo.id !== updatedTodo.id
-                );
-            }
-        },
-        changeAllStatus(state) {
-            const updatedTodos = state.todos.map((todo) => {
-                return { ...todo, completed: !todo.completed };
-            });
+            return todo;
+         });
+      },
+      deleteTodos(state) {
+         const sure = window.confirm("Delete tasks ?");
+         if (sure) {
+            const updatedTodos = [...state.todos];
+            state.todos = updatedTodos.filter(
+               (todo) => todo.completed === false
+            );
+         }
+      },
+      deleteTodo(state, action: PayloadAction<number>) {
+         const sure = window.confirm("Delete task ?");
+         if (sure) {
+            const updatedTodo = { ...state.todos[action.payload] };
+            state.todos = state.todos.filter(
+               (todo) => todo.id !== updatedTodo.id
+            );
+         }
+      },
+      changeAllStatus(state) {
+         const updatedTodos = state.todos.map((todo) => {
+            return { ...todo, completed: !todo.completed };
+         });
 
-            state.todos = updatedTodos;
-        },
-        addNewTodo(state, action: PayloadAction<string>) {
-            state.todos.push({
-                userId: state.todos.length + 1,
-                id: state.todos.length + 1,
-                title: action.payload,
-                completed: false,
-            });
-        },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchTodos.pending, (state) => {
-            state.loading = true;
-        });
-        builder.addCase(fetchTodos.fulfilled, (state, action) => {
-            state.todos = action.payload;
-            state.loading = false;
-        });
-    },
+         state.todos = updatedTodos;
+      },
+      addNewTodo(state, action: PayloadAction<string>) {
+         state.todos.push({
+            userId: state.todos.length + 1,
+            id: state.todos.length + 1,
+            title: action.payload,
+            completed: false,
+         });
+      },
+   },
+   extraReducers: (builder) => {
+      builder.addCase(fetchTodos.pending, (state) => {
+         state.loading = true;
+      });
+      builder.addCase(fetchTodos.fulfilled, (state, action) => {
+         state.todos = action.payload;
+         state.loading = false;
+      });
+   },
 });
 
 export const {
-    toggleItem,
-    deleteTodos,
-    changeAllStatus,
-    addNewTodo,
-    deleteTodo,
+   toggleItem,
+   deleteTodos,
+   changeAllStatus,
+   addNewTodo,
+   deleteTodo,
 } = todoSlice.actions;
 
 export default todoSlice.reducer;
